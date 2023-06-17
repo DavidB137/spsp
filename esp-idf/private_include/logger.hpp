17da2dedc9a60aaab3660470ae373aa73867fd70
Dávid Benko <davidbenko@davidbenko.dev>
2023-06-17 15:39:41 +0200

    Use logging macros instead of `Logger` class
    Variadic functions don't work with C macros, so just stick with them
    instead.

---
+++ esp-idf/private_include/logger.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file logger.hpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief Logger for SPSP
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "esp_log.h"
+
+#pragma once
+
+#define SPSP_LOGE(fmt, ...) ESP_LOGE(SPSP_LOG_TAG, fmt, __VA_ARGS__)
+#define SPSP_LOGW(fmt, ...) ESP_LOGW(SPSP_LOG_TAG, fmt, __VA_ARGS__)
+#define SPSP_LOGI(fmt, ...) ESP_LOGI(SPSP_LOG_TAG, fmt, __VA_ARGS__)
+#define SPSP_LOGD(fmt, ...) ESP_LOGD(SPSP_LOG_TAG, fmt, __VA_ARGS__)
