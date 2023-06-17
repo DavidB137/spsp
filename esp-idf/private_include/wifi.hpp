f6124af77e9c23bde3cf3370716b06c56fe818e7
Dávid Benko <davidbenko@davidbenko.dev>
2023-06-17 18:04:32 +0200

    ESP: implement WiFi

---
+++ esp-idf/private_include/wifi.hpp
@@ -0,0 +1,74 @@
+/**
+ * @file wifi.hpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief WiFi manager for ESP platform
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+
+#include <string>
+
+namespace SPSP
+{
+    const char* const WIFI_HOSTNAME_PREFIX = "spsp";
+
+    /**
+     * @brief WiFi manager for ESP platform (singleton)
+     * 
+     * Used by `SPSP::INode` classes.
+     * 
+     * In client modes, all roaming features and WPA3 are enabled.
+     */
+    class WiFi
+    {
+        std::string ssid;
+        std::string password;
+
+        WiFi() {}
+    public:
+        /**
+         * @brief Singleton instantiator
+         * 
+         * @return This instance
+         */
+        static WiFi& instance()
+        {
+            static WiFi inst;
+            return inst;
+        }
+
+        /**
+         * @brief Initializes WiFi
+         * 
+         * If SSID is zero-length (or not given), no net interface is created
+         * and WiFi is initalized in ESP-NOW-only mode.
+         * 
+         * @param ssid Service-set identifier
+         * @param password Password for given SSID
+         */
+        void init(std::string ssid = "", std::string password = "");
+
+        /**
+         * @brief Deinitializes WiFi
+         * 
+         */
+        void deinit();
+
+        /**
+         * @brief Gets current WiFi channel
+         * 
+         * @return Channel number
+         */
+        uint8_t getChannel();
+
+        /**
+         * @brief Sets current channel
+         * 
+         * @param ch Channel
+         */
+        void setChannel(uint8_t ch);
+    };
+} // namespace SPSP
---
+++ esp-idf/src/wifi.cpp
@@ -0,0 +1,114 @@
+/**
+ * @file wifi.cpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief WiFi manager for ESP platform
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include <cstring>
+#include <string>
+
+#include "esp_mac.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+
+#include "logger.hpp"
+#include "wifi.hpp"
+
+// Log tag
+static const char* SPSP_LOG_TAG = "SPSP/WiFi";
+
+namespace SPSP
+{
+    void WiFi::init(std::string ssid, std::string password)
+    {
+        SPSP_LOGI("Initializing...");
+
+        // Store given parameters
+        this->ssid = ssid;
+        this->password = password;
+
+        bool ssid_not_empty = ssid.length() > 0;
+
+        ESP_ERROR_CHECK(esp_netif_init());
+        ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+        // Create net interface
+        if (ssid_not_empty) {
+            esp_netif_t *netif = esp_netif_create_default_wifi_sta();
+
+            // Set hostname
+            std::string hostname = WIFI_HOSTNAME_PREFIX;
+            uint8_t mac[8];
+            char mac_str[16];
+            ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac));
+            sprintf(mac_str, "%02x%02x%02x%02x%02x%02x", MAC2STR(mac));
+            hostname += mac_str;
+            esp_netif_set_hostname(netif, hostname.c_str());
+        }
+
+        wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+        ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+        // Events
+        // TODO
+
+        // WiFi config
+        if (ssid_not_empty) {
+            wifi_config_t wifi_config = {};
+            strcpy((char*) wifi_config.sta.ssid, ssid.c_str());
+            strcpy((char*) wifi_config.sta.password, password.c_str());
+
+            // Enable roaming features
+            wifi_config.sta.rm_enabled = true;
+            wifi_config.sta.btm_enabled = true;
+            wifi_config.sta.mbo_enabled = true;
+            wifi_config.sta.ft_enabled = true;
+
+            // Enable security features
+            wifi_config.sta.owe_enabled = true;
+            wifi_config.sta.sae_pwe_h2e = WPA3_SAE_PWE_BOTH;
+
+            ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+        }
+
+        // Set station mode and start WiFi
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+        ESP_ERROR_CHECK(esp_wifi_start());
+
+        // No power saving for bridge (IMPORTANT!)
+        if (ssid_not_empty) {
+            esp_wifi_set_ps(WIFI_PS_NONE);
+        }
+
+        SPSP_LOGI("Initialized");
+    }
+
+    void WiFi::deinit()
+    {
+        SPSP_LOGI("Deinitializing...");
+
+        ESP_ERROR_CHECK(esp_wifi_stop());
+        ESP_ERROR_CHECK(esp_wifi_deinit());
+        ESP_ERROR_CHECK(esp_event_loop_delete_default());
+        //ESP_ERROR_CHECK(esp_netif_deinit());
+
+        SPSP_LOGI("Deinitialized");
+    }
+
+    uint8_t WiFi::getChannel()
+    {
+        uint8_t ch;
+        wifi_second_chan_t sec;
+        ESP_ERROR_CHECK(esp_wifi_get_channel(&ch, &sec));
+        return ch;
+    }
+
+    void WiFi::setChannel(uint8_t ch)
+    {
+        ESP_ERROR_CHECK(esp_wifi_set_channel(ch, WIFI_SECOND_CHAN_NONE));
+        SPSP_LOGI("Set channel %d", ch);
+    }
+} // namespace SPSP
