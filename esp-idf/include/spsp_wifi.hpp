a49453555dfe6a8c831a551701b44437125acc58
Dávid Benko <davidbenko@davidbenko.dev>
2023-06-19 16:32:31 +0200

    ESP/WiFi: rework
    WiFi works now.
    - split initialization code
    - enhance event handling
    - do full scans instead of fast scans (bridge only)
    - add mutexes
    - block initialization until IP is acquired (bridge only)

--- esp-idf/include/spsp_wifi.hpp
+++ esp-idf/include/spsp_wifi.hpp
@@ -9,26 +9,44 @@
 
 #pragma once
 
+#include <chrono>
+#include <exception>
+#include <future>
+#include <mutex>
 #include <string>
+
+#include "esp_event.h"
 
 namespace SPSP
 {
-    const char* const WIFI_HOSTNAME_PREFIX = "spsp-";
+    const char* const WIFI_HOSTNAME_PREFIX = "spsp-";         //!< Hostname prefix
+    const auto WIFI_INIT_TIMEOUT = std::chrono::seconds(20);  //!< Timeout for connecting to AP
+
+    /**
+     * @brief WiFi connection error exception
+     * 
+     * Thrown when `WIFI_INIT_TIMEOUT` expires before successful connection.
+     */
+    class WiFiConnectionError : public std::exception {};
 
     /**
      * @brief WiFi manager for ESP platform (singleton)
      * 
-     * Used by `SPSP::INode` classes.
+     * Must be initialized manually before instantiating ESP-NOW and MQTT.
+     * (And deinitialized also manually.)
      * 
-     * In client modes, all roaming features and WPA3 are enabled.
+     * In WiFi station mode, all roaming features and WPA3 are enabled.
      */
     class WiFi
     {
-        std::string m_ssid = "";
-        std::string m_password = "";
-        bool initialized = false;
+        std::string m_ssid = "";                 //!< WiFi SSID
+        std::string m_password = "";             //!< WiFi Password
+        bool m_initialized = false;              //!< Whether WiFi is initialized
+        std::promise<void> m_connectingPromise;  //!< Promise to block until successful connection is made
+        std::mutex m_mutex;                      //!< Mutex to prevent race conditions (primarily for initialization)
 
         WiFi() {}
+
     public:
         /**
          * @brief Returns instance of this singleton class
@@ -47,7 +65,9 @@ namespace SPSP
          * If SSID is zero-length (or not given), no net interface is created
          * and WiFi is initalized in ESP-NOW-only mode.
          * 
-         * May be called multiple times.
+         * May be called multiple times and is multi-thread safe.
+         * Blocks until connection is established. May throw
+         * `WiFiConnectionError`.
          * 
          * @param ssid Service-set identifier
          * @param password Password for given SSID
@@ -74,5 +94,59 @@ namespace SPSP
          * @param ch Channel
          */
         void setChannel(uint8_t ch);
+
+    private:
+        /**
+         * @brief Initializes NVS
+         * 
+         * Called from `init()`.
+         */
+        void initNVS();
+
+        /**
+         * @brief Initializes network interface.
+         * 
+         * Called from `init()`.
+         */
+        void initNetIf();
+
+        /**
+         * @brief Registers WiFi event handlers.
+         * 
+         * Called from `init()`.
+         */
+        void registerEventHandlers();
+
+        /**
+         * @brief Initializes WiFi configuration to match current parameters.
+         * 
+         * Called from `init()`.
+         */
+        void initWiFiConfig();
+
+        /**
+         * @brief Event handler for WiFi
+         * 
+         */
+
+        /**
+         * @brief Event handler for WiFi
+         * 
+         * @param ctx Context (pointer to this instance)
+         * @param eventBase Event base (`WIFI_EVENT`)
+         * @param eventId Event ID
+         * @param eventData Data of that event
+         */
+        static void eventHandlerWiFi(void* ctx, esp_event_base_t eventBase, int32_t eventId, void* eventData);
+
+        /**
+         * @brief Event handler for IP
+         * 
+         * @param ctx Context (pointer to this instance)
+         * @param eventBase Event base (`IP_EVENT`)
+         * @param eventId Event ID
+         * @param eventData Data of that event
+         */
+        static void eventHandlerIP(void* ctx, esp_event_base_t eventBase, int32_t eventId, void* eventData);
     };
 } // namespace SPSP
--- esp-idf/src/wifi.cpp
+++ esp-idf/src/wifi.cpp
@@ -25,52 +25,116 @@ namespace SPSP
 {
     void WiFi::init(const std::string ssid, const std::string password)
     {
+        // Mutex
+        const std::lock_guard<std::mutex> lock(m_mutex);
+
         // Don't do anything if already initialized
-        if (this->initialized) return;
+        if (this->m_initialized) return;
 
         // Store given parameters
         this->m_ssid = ssid;
         this->m_password = password;
 
-        bool ssidNotEmpty = ssid.length() > 0;
+        // Create event loop
+        ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-        // Initialize NVS
+        // Initialize NVS and network
+        this->initNVS();
+        this->initNetIf();
+
+        // Register handlers
+        this->registerEventHandlers();
+
+        // Init configuration
+        wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+        ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+        ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+
+        // Use current config settings
+        this->initWiFiConfig();
+
+        // Start WiFi
+        ESP_ERROR_CHECK(esp_wifi_start());
+
+        // Wait until IP is received (only if SSID is not empty - i.e. this is
+        // bridge node)
+        if (m_ssid.length() > 0) {
+            auto future = m_connectingPromise.get_future();
+
+            SPSP_LOGI("Attempting connection with timeout %lld seconds",
+                WIFI_INIT_TIMEOUT.count());
+
+            // Block
+            if (future.wait_for(WIFI_INIT_TIMEOUT) == std::future_status::timeout) {
+                // Connection timeout
+                SPSP_LOGE("Connection timeout");
+                m_mutex.unlock();
+                throw WiFiConnectionError();
+            }
+        }
+
+        this->m_initialized = true;
+
+        SPSP_LOGI("Initialized");
+    }
+
+    void WiFi::initNVS()
+    {
         esp_err_t nvsInitCode = nvs_flash_init();
+
         if (nvsInitCode == ESP_ERR_NVS_NO_FREE_PAGES || nvsInitCode == ESP_ERR_NVS_NEW_VERSION_FOUND) {
             ESP_ERROR_CHECK(nvs_flash_erase());
             nvsInitCode = nvs_flash_init();
         }
-        ESP_ERROR_CHECK(nvsInitCode);
-        SPSP_LOGI("NVS initialized");
 
+        ESP_ERROR_CHECK(nvsInitCode);
+
+        SPSP_LOGI("NVS initialized");
+    }
+
+    void WiFi::initNetIf()
+    {
         ESP_ERROR_CHECK(esp_netif_init());
-        ESP_ERROR_CHECK(esp_event_loop_create_default());
 
         // Create net interface
-        if (ssidNotEmpty) {
-            esp_netif_t *netif = esp_netif_create_default_wifi_sta();
+        esp_netif_t* netif = esp_netif_create_default_wifi_sta();
 
-            // Set hostname
-            std::string hostname = WIFI_HOSTNAME_PREFIX;
-            uint8_t mac[8];
-            char macStr[16];
-            ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac));
-            sprintf(macStr, "%02x%02x%02x%02x%02x%02x", MAC2STR(mac));
-            hostname += macStr;
-            esp_netif_set_hostname(netif, hostname.c_str());
-        }
+        // Set hostname
+        std::string hostname = WIFI_HOSTNAME_PREFIX;
+        uint8_t mac[8];
+        char macStr[16];
+        ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac));
+        sprintf(macStr, "%02x%02x%02x%02x%02x%02x", MAC2STR(mac));
+        hostname += macStr;
+        esp_netif_set_hostname(netif, hostname.c_str());
 
-        wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-        ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+        SPSP_LOGI("Network interface initialized");
+    }
 
-        // Events
-        // TODO
+    void WiFi::registerEventHandlers()
+    {
+        // WiFi events
+        ESP_ERROR_CHECK(esp_event_handler_instance_register(
+            WIFI_EVENT, ESP_EVENT_ANY_ID, &WiFi::eventHandlerWiFi,
+            this, nullptr));
 
-        // WiFi config
-        if (ssidNotEmpty) {
+        // IP events
+        ESP_ERROR_CHECK(esp_event_handler_instance_register(
+            IP_EVENT, ESP_EVENT_ANY_ID, &WiFi::eventHandlerIP,
+            this, nullptr));
+    }
+
+    void WiFi::initWiFiConfig()
+    {
+        // SSID is not empty (this is bridge node)
+        if (m_ssid.length() > 0) {
             wifi_config_t wifi_config = {};
-            strcpy((char*) wifi_config.sta.ssid, ssid.c_str());
-            strcpy((char*) wifi_config.sta.password, password.c_str());
+            strcpy((char*) wifi_config.sta.ssid, m_ssid.c_str());
+            strcpy((char*) wifi_config.sta.password, m_password.c_str());
+
+            // Do full scan - connect to AP with strongest signal
+            wifi_config.sta.scan_method = WIFI_ALL_CHANNEL_SCAN;
 
             // Enable roaming features
             wifi_config.sta.rm_enabled = true;
@@ -83,31 +147,25 @@ namespace SPSP
             wifi_config.sta.sae_pwe_h2e = WPA3_SAE_PWE_BOTH;
 
             ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-        }
 
-        // Set station mode and start WiFi
-        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-        ESP_ERROR_CHECK(esp_wifi_start());
-
-        // No power saving for bridge (IMPORTANT!)
-        if (ssidNotEmpty) {
+            // No power saving for bridge (IMPORTANT!)
             esp_wifi_set_ps(WIFI_PS_NONE);
         }
-
-        this->initialized = true;
-
-        SPSP_LOGI("Initialized");
     }
 
     void WiFi::deinit()
     {
+        // Mutex
+        const std::lock_guard<std::mutex> lock(m_mutex);
+
         // Don't do anything if already deinitialized
-        if (!this->initialized) return;
+        if (!this->m_initialized) return;
 
         ESP_ERROR_CHECK(esp_wifi_stop());
         ESP_ERROR_CHECK(esp_wifi_deinit());
         ESP_ERROR_CHECK(esp_event_loop_delete_default());
-        //ESP_ERROR_CHECK(esp_netif_deinit());
+
+        this->m_initialized = false;
 
         SPSP_LOGI("Deinitialized");
     }
@@ -122,7 +180,57 @@ namespace SPSP
 
     void WiFi::setChannel(uint8_t ch)
     {
+        // Mutex
+        const std::lock_guard<std::mutex> lock(m_mutex);
+
         ESP_ERROR_CHECK(esp_wifi_set_channel(ch, WIFI_SECOND_CHAN_NONE));
         SPSP_LOGI("Set channel %d", ch);
+    }
+    
+    void WiFi::eventHandlerWiFi(void* ctx, esp_event_base_t, int32_t eventId, void* eventData)
+    {
+        switch (eventId) {
+        case WIFI_EVENT_STA_START:
+        case WIFI_EVENT_STA_DISCONNECTED:
+            esp_wifi_connect();
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+    void WiFi::eventHandlerIP(void* ctx, esp_event_base_t, int32_t eventId, void* eventData)
+    {
+        // Get "this"
+        WiFi* inst = static_cast<WiFi*>(ctx);
+
+        ip_event_got_ip_t* event_got_ip4;
+        ip_event_got_ip6_t* event_got_ip6;
+
+        switch (eventId) {
+        case IP_EVENT_STA_GOT_IP:
+            // Got IPv4 address
+            event_got_ip4 = static_cast<ip_event_got_ip_t*>(eventData);
+            SPSP_LOGI("Got IP: " IPSTR, IP2STR(&event_got_ip4->ip_info.ip));
+
+            // Resolve promise
+            inst->m_connectingPromise.set_value();
+
+            break;
+        
+        case IP_EVENT_GOT_IP6:
+            // Got IPv6 address
+            event_got_ip6 = static_cast<ip_event_got_ip6_t*>(eventData);
+            SPSP_LOGI("Got IPv6: " IPV6STR, IPV62STR(event_got_ip6->ip6_info.ip));
+
+            // Resolve promise
+            inst->m_connectingPromise.set_value();
+
+            break;
+        
+        default:
+            break;
+        }
     }
 } // namespace SPSP
