538cfb1f858f82b4897dfb716951343a1b691245
Dávid Benko <davidbenko@davidbenko.dev>
2023-06-21 18:44:23 +0200

    ESP-NOW: complete rework
    - implement unimplemented functionality
    - add mutexes and overall multi-thread safety

--- esp-idf/src/espnow.cpp
+++ esp-idf/src/espnow.cpp
@@ -7,8 +7,16 @@
  * 
  */
 
-#include "spsp_logger.hpp"
+#include <cstring>
+
+#include "esp_mac.h"
+#include "esp_now.h"
+#include "esp_random.h"
+
+#include "spsp_chacha20.hpp"
 #include "spsp_espnow.hpp"
+#include "spsp_logger.hpp"
+#include "spsp_node.hpp"
 #include "spsp_wifi.hpp"
 
 // Log tag
@@ -16,27 +24,302 @@ static const char* SPSP_LOG_TAG = "SPSP/Local/ESPNOW";
 
 namespace SPSP::LocalLayers::ESPNOW
 {
-    Layer::Layer(const char ssid[SSID_LEN], const char password[PASSWORD_LEN], uint8_t datarate)
-        : m_ssid{ssid}, m_password{password}, m_datarate{datarate}
+    // Instance pointer
+    // ESP-NOW callbacks don't take `void*` context pointers, so we have to get
+    // creative.
+    static Layer* espnowInstance = nullptr;
+
+    // Wrapper for C receive callback
+    void _receiveCallback(const esp_now_recv_info_t* espnowInfo, const uint8_t* data, int dataLen)
     {
-        // WiFi
-        WiFi& wifi = WiFi::getInstance();
-        wifi.init();
+        espnowInstance->receiveCallback(espnowInfo->src_addr,
+            const_cast<uint8_t*>(data), dataLen, espnowInfo->rx_ctrl->rssi);
+    }
+
+    // Wrapper for C send callback
+    void _sendCallback(const uint8_t *dst, esp_now_send_status_t status)
+    {
+        espnowInstance->sendCallback(dst, status == ESP_NOW_SEND_SUCCESS);
+    }
+
+    Layer::Layer(uint32_t ssid, const std::string password)
+        : m_ssid{ssid}, m_password{password}
+    {
+        // Store pointer to this instance
+        // See above
+        espnowInstance = this;
+
+        // Initialize
+        ESP_ERROR_CHECK(esp_now_init());
+
+        // Register callbacks
+        ESP_ERROR_CHECK(esp_now_register_recv_cb(_receiveCallback));
+        ESP_ERROR_CHECK(esp_now_register_send_cb(_sendCallback));
 
         SPSP_LOGI("Initialized");
     }
 
     Layer::~Layer()
     {
-        // WiFi
-        WiFi& wifi = WiFi::getInstance();
-        wifi.deinit();
-
+        ESP_ERROR_CHECK(esp_now_deinit());
         SPSP_LOGI("Deinitialized");
     }
 
-    bool Layer::send(const Message msg)
+    bool Layer::send(const LocalMessage msg)
     {
+        unsigned dataLen = sizeof(Packet) + msg.topic.length() + msg.payload.length();
+
+        if (!this->validateMessage(msg)) return false;
+
+        uint8_t* data = new uint8_t[dataLen];
+        this->preparePacket(msg, data);
+
+        SPSP_LOGD("Sending %u bytes to %s", dataLen, msg.src.str.c_str());
+
+        // Promise/mutex bucket
+        auto bucketId = this->getBucketIdFromLocalAddr(msg.src);
+
+        // Lock both `m_mutex` and MAC's `m_sendingMutexes` entry without
+        // deadlock
+        std::lock(m_mutex, m_sendingMutexes[bucketId]);
+
+        auto future = m_sendingPromises[bucketId].get_future();
+
+        // Send
+        this->sendRaw(msg.src, data, dataLen);
+
+        m_mutex.unlock();
+
+        // Free memory
+        delete[] data;
+ 
+        // Wait for callback to finish
+        bool delivered = future.get();
+
+        // Reset promise
+        m_sendingPromises[bucketId] = std::promise<bool>{};
+
+        m_sendingMutexes[bucketId].unlock();
+
+        SPSP_LOGD("Sent %u bytes to %s: %s", dataLen, msg.src.str.c_str(),
+                  delivered ? "success" : "fail");
+
+        return delivered;
+    }
+
+    void Layer::sendRaw(const LocalAddr& dst, const uint8_t* data, size_t dataLen)
+    {
+        // Register peer
+        this->registerPeer(dst);
+
+        // Get MAC address
+        esp_now_peer_info_t peerInfo = {};
+        this->localAddrToMac(dst, peerInfo.peer_addr);
+
+        // Send
+        ESP_ERROR_CHECK(esp_now_send(peerInfo.peer_addr, data, dataLen));
+
+        // Unregister peer
+        this->registerPeer(dst);
+    }
+
+    bool Layer::validateMessage(const LocalMessage msg) const
+    {
+        unsigned dataLen = sizeof(Packet) + msg.topic.length() + msg.payload.length();
+
+        if (dataLen > ESP_NOW_MAX_DATA_LEN) {
+            SPSP_LOGE("Send fail: packet too big");
+            return false;
+        }
+
         return true;
+    }
+
+    void Layer::preparePacket(const LocalMessage msg, uint8_t* data) const
+    {
+        uint8_t topicLen = msg.topic.length();
+        uint8_t payloadLen = msg.payload.length();
+        unsigned dataLen = sizeof(Packet) + topicLen + payloadLen;
+
+        Packet* p = reinterpret_cast<Packet*>(data);
+
+        // Fill data
+        p->header.ssid = m_ssid;
+        p->header.version = PROTO_VERSION;
+        p->payload.type = msg.type;
+        p->payload.checksum = 0;
+        p->payload.topicLen = topicLen;
+        p->payload.payloadLen = payloadLen;
+
+        // Copy topic and payload
+        memcpy(p->payload.topicAndPayload, msg.topic.c_str(), topicLen);
+        memcpy(p->payload.topicAndPayload + topicLen, msg.payload.c_str(),
+               payloadLen);
+
+        // Generate nonce
+        esp_fill_random(&(p->header.nonce), NONCE_LEN);
+
+        // Skip header
+        data += sizeof(PacketHeader);
+        dataLen -= sizeof(PacketHeader);
+
+        // Checksum
+        p->payload.checksum = this->checksumRaw(data, dataLen);
+
+        // Encrypt
+        this->encryptRaw(data, dataLen, p->header.nonce);
+    }
+
+    void Layer::registerPeer(const LocalAddr& addr)
+    {
+        // Get MAC address
+        esp_now_peer_info_t peerInfo = {};
+        this->localAddrToMac(addr, peerInfo.peer_addr);
+
+        ESP_ERROR_CHECK(esp_now_add_peer(&peerInfo));
+    }
+
+    void Layer::unregisterPeer(const LocalAddr& addr)
+    {
+        // Get MAC address
+        esp_now_peer_info_t peerInfo = {};
+        this->localAddrToMac(addr, peerInfo.peer_addr);
+
+        ESP_ERROR_CHECK(esp_now_del_peer(peerInfo.peer_addr));
+    }
+
+    void Layer::receiveCallback(const uint8_t* src, uint8_t* data, unsigned dataLen, int rssi) const
+    {
+        SPSP_LOGD("Receive: packet from %s", macTolocalAddr(src).str.c_str());
+
+        // Check packet length
+        if (dataLen < sizeof(Packet)) {
+            SPSP_LOGD("Receive fail: packet too short");
+            return;
+        }
+
+        // Treat as `Packet`
+        Packet* p = reinterpret_cast<Packet*>(data);
+
+        // Validate and decrypt
+        if (!this->validatePacketHeader(p)) return;
+        if (!this->decryptAndValidatePacketPayload(data, dataLen)) return;
+
+        const char* topicAndPayload = reinterpret_cast<const char*>(p->payload.topicAndPayload);
+
+        // Construct message
+        LocalMessage msg;
+        msg.type = p->payload.type;
+        msg.src = this->macTolocalAddr(src);
+        msg.topic = std::string{topicAndPayload, p->payload.topicLen};
+        msg.payload = std::string{topicAndPayload + p->payload.topicLen,
+                              p->payload.payloadLen};
+
+        // Send to the Node
+        if (this->nodeConnected()) {
+            this->getNode()->receiveLocal(msg, rssi);
+        }
+    }
+
+    bool Layer::validatePacketHeader(const Packet* p) const
+    {
+        // Check SSID
+        if (p->header.ssid != m_ssid) {
+            SPSP_LOGD("Receive fail: different SSID");
+            return false;
+        }
+
+        // Check version
+        if (p->header.version != PROTO_VERSION) {
+            SPSP_LOGD("Receive fail: different protocol version");
+            return false;
+        }
+
+        return true;
+    }
+
+    bool Layer::decryptAndValidatePacketPayload(uint8_t* data, unsigned dataLen) const
+    {
+        // Treat as `Packet`
+        Packet* p = reinterpret_cast<Packet*>(data);
+
+        // Skip header
+        data += sizeof(PacketHeader);
+        dataLen -= sizeof(PacketHeader);
+
+        // Decrypt
+        this->encryptRaw(data, dataLen, p->header.nonce);
+
+        // Check checksum
+        uint8_t checksum = this->checksumRaw(data, dataLen, p->payload.checksum);
+
+        if (checksum != p->payload.checksum) {
+            SPSP_LOGD("Receive fail: invalid checksum");
+            return false;
+        }
+
+        // Assert valid payload length
+        if (sizeof(PacketPayload) + p->payload.topicLen + p->payload.payloadLen != dataLen) {
+            SPSP_LOGD("Receive fail: invalid total length");
+            return false;
+        }
+
+        return true;
+    }
+
+    void Layer::sendCallback(const uint8_t* dst, bool delivered)
+    {
+        // Promise/mutex bucket
+        auto bucketId = this->getBucketIdFromLocalAddr(this->macTolocalAddr(dst));
+
+        m_sendingPromises[bucketId].set_value(delivered);
+    }
+
+    const LocalAddr Layer::macTolocalAddr(const uint8_t* mac)
+    {
+        LocalAddr la;
+
+        // Internal representation
+        la.addr = std::vector<uint8_t>(mac, mac + ESP_NOW_ETH_ALEN);
+
+        // Printable string
+        char macStr[16];
+        sprintf(macStr, "%02x%02x%02x%02x%02x%02x", MAC2STR(mac));
+        la.str = std::string(macStr);
+
+        return la;
+    }
+
+    void Layer::localAddrToMac(const LocalAddr& la, uint8_t* mac)
+    {
+        for (unsigned i = 0; i < la.addr.size(); i++) {
+            mac[i] = la.addr[i];
+        }
+    }
+
+    void Layer::encryptRaw(uint8_t* data, unsigned dataLen, const uint8_t* nonce) const
+    {
+        Chacha20 chch{
+            reinterpret_cast<const uint8_t*>(m_password.c_str()),
+            nonce
+        };
+
+        chch.crypt(reinterpret_cast<uint8_t*>(data), dataLen);
+    }
+
+    uint8_t Layer::checksumRaw(uint8_t* data, unsigned dataLen, uint8_t existingChecksum)
+    {
+        uint8_t checksum = 0;
+
+        for (unsigned i = 0; i < dataLen; i++) {
+            checksum += data[i];
+        }
+
+        return checksum - existingChecksum;
+    }
+
+    uint8_t Layer::getBucketIdFromLocalAddr(const LocalAddr& addr) const
+    {
+        return std::hash<LocalAddr>{}(addr) % this->m_sendingPromises.size();
     }
 } // namespace SPSP::LocalLayers::ESPNOW
--- include/spsp_espnow.hpp
+++ include/spsp_espnow.hpp
@@ -9,15 +9,62 @@
 
 #pragma once
 
+#include <array>
+#include <future>
+#include <mutex>
 #include <string>
 
 #include "spsp_layers.hpp"
 
 namespace SPSP::LocalLayers::ESPNOW
 {
-    static const uint8_t PROTO_VERSION = 2;   //!< Current protocol version
-    static const uint8_t SSID_LEN      = 8;   //!< SSID length in bytes
+    static const uint8_t PROTO_VERSION = 1;   //!< Current protocol version
     static const uint8_t PASSWORD_LEN  = 32;  //!< Password length in bytes
+    static const uint8_t NONCE_LEN     = 8;   //!< Length of encryption nonce
+
+    /**
+     * @brief Maximum number of simultaneous peers
+     * 
+     * Peers are added and removed during each message sending, so this really
+     * only limits number of concurrent "deliveries". Concurrent "deliveries"
+     * over this limit will have to wait in queue.
+     */
+    static const uint8_t MAX_PEER_NUM = 15;
+
+    #pragma pack(push, 1)
+    /**
+     * @brief ESP-NOW packet header
+     * 
+     * Constains SSID and encryption nonce.
+     */
+    struct PacketHeader
+    {
+        uint32_t ssid;              //!< Service set identifier
+        uint8_t nonce[NONCE_LEN];   //!< Encryption nonce
+        uint8_t version;            //!< Current protocol version
+    };
+
+    struct PacketPayload
+    {
+        LocalMessageType type;      //!< Message type
+        uint8_t _reserved;          //!< Reserved for future use
+        uint8_t checksum;           //!< Simple checksum of `PacketPayload` to validate decrypted packet
+        uint8_t topicLen;           //!< Length of topic
+        uint8_t payloadLen;         //!< Length of payload (data)
+        uint8_t topicAndPayload[];  //!< Topic and payload as string (not null terminated)
+    };
+
+    struct Packet
+    {
+        PacketHeader header;
+        PacketPayload payload;
+    };
+    #pragma pack(pop)
+
+    // Assert sizes
+    static_assert(sizeof(PacketHeader) == 13);
+    static_assert(sizeof(PacketPayload) == 5);
+    static_assert(sizeof(Packet) == 18);
 
     /**
      * @brief ESP-NOW local layer
@@ -26,21 +73,44 @@ namespace SPSP::LocalLayers::ESPNOW
     class Layer : public SPSP::ILocalLayer
     {
     private:
-        std::string m_ssid;
+        uint32_t m_ssid;
         std::string m_password;
-        uint8_t m_datarate;
+
+        std::mutex m_mutex;  //!< Mutex to prevent race conditions
+
+        /**
+         * @brief Container for promises of being-sent messages
+         * 
+         * `send()` blocks until status of delivery is available (and no other
+         * message is being sent to the same node).
+         * Assign each peer a "bucket". When sending a message to other peer
+         * from the same bucket, wait for it to finish. Additional support for
+         * this mechanism is provided by `m_sendingMutexes`.
+         */
+        std::array<std::promise<bool>, MAX_PEER_NUM> m_sendingPromises;
+
+        /**
+         * @brief Container for mutex of being-sent messages
+         * 
+         * We need only one thread waiting for message status per bucket in
+         * `m_sendingPromises`.
+         * Efectively allows maximum of `MAX_PEER_NUM` messages being sent
+         * at the same time.
+         */
+        std::array<std::mutex, MAX_PEER_NUM> m_sendingMutexes;
 
     public:
         /**
          * @brief Constructs a new ESP-NOW layer object
          * 
-         * Also initializes WiFi (if not already initialized).
+         * Requires already initialized WiFi.
+         * 
+         * Only one instance may exist at the same time.
          * 
          * @param ssid Service-set identifier
-         * @param password Encryption password for communication
-         * @param datarate In case of ESP-IDF, use values of `wifi_phy_rate_t` enum.
+         * @param password Encryption password for communication (32 bytes)
          */
-        Layer(const char ssid[SSID_LEN], const char password[PASSWORD_LEN], uint8_t datarate = 0);
+        Layer(uint32_t ssid, const std::string password);
 
         /**
          * @brief Destroys ESP-NOW layer object
@@ -48,14 +118,159 @@ namespace SPSP::LocalLayers::ESPNOW
          */
         ~Layer();
 
+        /**
+         * @brief Receive callback for underlaying ESP-NOW C functions.
+         * 
+         * Used indirectly.
+         * 
+         * @param src Source address
+         * @param data Raw data
+         * @param dataLen Length of data
+         * @param rssi Received signal strength indicator (in dBm)
+         */
+        void receiveCallback(const uint8_t* src, uint8_t* data, unsigned dataLen, int rssi) const;
+
+        /**
+         * @brief Send callback for underlaying ESP-NOW C functions.
+         * 
+         * Used indirectly.
+         * 
+         * @param dst Destination address
+         * @param delivered Whether packet has been delivered successfully
+         */
+        void sendCallback(const uint8_t* dst, bool delivered);
+
+        /**
+         * @brief Converts MAC address to `LocalAddr` instance
+         * 
+         * @param mac MAC address pointer
+         * @return `LocalAddr` instance
+         */
+        static const LocalAddr macTolocalAddr(const uint8_t* mac);
+
+        /**
+         * @brief Converts `LocalAddr` instance to MAC address
+         * 
+         * @param la `LocalAddr` instance
+         * @param mac MAC address pointer (modified in-place)
+         * @return MAC address pointer
+         */
+        static void localAddrToMac(const LocalAddr& la, uint8_t* mac);
+
     protected:
         /**
          * @brief Sends the message to given node
+         * 
+         * Note: this will block until delivery status is confirmed!
          * 
          * @param msg Message
          * @return true Delivery successful
          * @return false Delivery failed
          */
-        bool send(const Message msg);
+        bool send(const LocalMessage msg);
+
+        /**
+         * @brief Sends raw packet to the underlaying library
+         * 
+         * Also registers and unregisters the peer temporarily.
+         * 
+         * This is not multi-thread safe.
+         * 
+         * @param dst Destination address
+         * @param data Raw data
+         * @param dataLen Length of data
+         */
+        void sendRaw(const LocalAddr& dst, const uint8_t* data, size_t dataLen);
+        
+        /**
+         * @brief Encryption provider for raw data (bytes)
+         * 
+         * Wraps ChaCha20 encryption.
+         * This function is used for both encryption and decryption.
+         * Data are encrypted/decrypted in-place.
+         * 
+         * @param data Data to encrypt/decrypt
+         * @param dataLen Length of data
+         * @param nonce Encryption nonce
+         */
+        void encryptRaw(uint8_t* data, unsigned dataLen, const uint8_t* nonce) const;
+
+        /**
+         * @brief Validates packet's header
+         * 
+         * @param p Packet
+         * @return true Packet header is valid
+         * @return false Packet header is invalid
+         */
+        bool validatePacketHeader(const Packet* p) const;
+
+        /**
+         * @brief Decrypts and validates packet's payload
+         * 
+         * @param data Raw packet data
+         * @param dataLen Length of data
+         * @return true Packet header is valid
+         * @return false Packet header is invalid
+         */
+        bool decryptAndValidatePacketPayload(uint8_t* data, unsigned dataLen) const;
+
+        /**
+         * @brief Validates whether message can be sent
+         * 
+         * @param msg Message
+         * @return true Message can be sent
+         * @return false Message can't be sent
+         */
+        bool validateMessage(const LocalMessage msg) const;
+
+        /**
+         * @brief Prepares packet to be sent
+         * 
+         * Populates raw data with Packet object.
+         * 
+         * @param msg Message
+         * @param data Raw memory for Packet
+         */
+        void preparePacket(const LocalMessage msg, uint8_t* data) const;
+
+        /**
+         * @brief Registers given peer
+         * 
+         * This is not multi-thread safe.
+         * 
+         * @param addr Address of the peer
+         */
+        void registerPeer(const LocalAddr& addr);
+
+        /**
+         * @brief Unregisters given peer
+         * 
+         * This is not multi-thread safe.
+         * 
+         * @param addr Address of the peer
+         */
+        void unregisterPeer(const LocalAddr& addr);
+
+        /**
+         * @brief Checksums the given raw data (bytes)
+         * 
+         * Optionally subtracts existing checksum.
+         * 
+         * @param data Data to encrypt/decrypt
+         * @param dataLen Length of data
+         * @param existingChecksum Existing checksum (to subtract)
+         * @return Correct checksum
+         */
+        static uint8_t checksumRaw(uint8_t* data, unsigned dataLen, uint8_t existingChecksum = 0);
+
+        /**
+         * @brief Calculates bucket id from `LocalAddr` object
+         * 
+         * Used for `m_sendingPromises` and `m_sendingMutexes` arrays.
+         * 
+         * @param addr Address of the peer
+         * @return Bucket id
+         */
+        uint8_t getBucketIdFromLocalAddr(const LocalAddr& addr) const;
     };
 } // namespace SPSP::LocalLayers::ESPNOW
