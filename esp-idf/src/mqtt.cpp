f9eb0ef97c79799e3791822ed43eff495cf38189
Dávid Benko <davidbenko@davidbenko.dev>
2023-07-05 09:44:12 +0200

    MQTT: add placeholder logs (WIP)

--- esp-idf/src/mqtt.cpp
+++ esp-idf/src/mqtt.cpp
@@ -29,16 +29,20 @@ namespace SPSP::FarLayers::MQTT
     bool Layer::publish(const std::string src, const std::string topic,
                         const std::string payload)
     {
+        SPSP_LOGD("Publish: payload %s to topic %s from %s",
+                  payload.c_str(), topic.c_str(), src.c_str());
         return true;
     }
 
     bool Layer::subscribe(const std::string topic)
     {
+        SPSP_LOGD("Subscribe: %s", topic.c_str());
         return true;
     }
 
     bool Layer::unsubscribe(const std::string topic)
     {
+        SPSP_LOGD("Unsubscribe: %s", topic.c_str());
         return true;
     }
 }
