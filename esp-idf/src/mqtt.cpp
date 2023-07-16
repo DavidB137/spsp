1733c4e56eb814e1c148c47a89ad6af11c97f249
Dávid Benko <davidbenko@davidbenko.dev>
2023-07-16 21:50:31 +0200

    MQTT: add initial implementation

--- esp-idf/src/mqtt.cpp
+++ esp-idf/src/mqtt.cpp
@@ -7,6 +7,11 @@
  * 
  */
 
+#include <thread>
+
+#include "esp_mac.h"
+#include "mqtt_client.h"
+
 #include "spsp_logger.hpp"
 #include "spsp_mqtt.hpp"
 #include "spsp_wifi.hpp"
@@ -16,33 +21,171 @@ static const char* SPSP_LOG_TAG = "SPSP/Far/MQTT";
 
 namespace SPSP::FarLayers::MQTT
 {
-    Layer::Layer()
+    void _eventHandler(void *ctx, esp_event_base_t, int32_t eventId, void *eventData)
     {
+        auto mqtt = static_cast<Layer*>(ctx);
+        auto event = static_cast<esp_mqtt_event_handle_t>(eventData);
+
+        std::string topic;
+        std::string data;
+
+        switch (eventId) {
+        case MQTT_EVENT_CONNECTED:
+            SPSP_LOGD("Connected");
+            mqtt->connected();
+            break;
+
+        case MQTT_EVENT_DISCONNECTED:
+            SPSP_LOGD("Disconnected, reconnecting...");
+            mqtt->reconnect();
+            break;
+
+        case MQTT_EVENT_DATA:
+            // TODO: implement support for multiple chunks of data
+            if (event->current_data_offset > 0) return;
+
+            topic = std::string(event->topic, event->topic_len);
+            data = std::string(event->data, event->data_len);
+
+            SPSP_LOGD("Received data '%s' on topic '%s'",
+                      data.c_str(), topic.c_str());
+
+            if (mqtt->nodeConnected()) {
+                // Create new thread
+                std::thread t([mqtt, topic, data] {
+                    mqtt->getNode()->receiveFar(topic, data);
+                });
+
+                // Run independently
+                t.detach();
+            }
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    Layer::Layer(const ClientConfig config)
+        : m_pubTopicPrefix{config.pubTopicPrefix}, m_qos{config.connection.qos},
+          m_retain{config.connection.retain}
+    {
+        // Client ID
+        std::string clientId;
+
+        if (config.auth.clientId.empty()) {
+            clientId = MQTT_CLIENT_ID_PREFIX;
+
+            uint8_t mac[8];
+            char macStr[16];
+            ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac));
+            sprintf(macStr, "%02x%02x%02x%02x%02x%02x", MAC2STR(mac));
+
+            clientId += macStr;
+        } else {
+            clientId = config.auth.clientId;
+        }
+
+        // Create ESP config
+        esp_mqtt_client_config_t espConfig = {};
+
+        espConfig.broker.address.uri = config.connection.uri.c_str();
+        espConfig.broker.verification.certificate = this->stringToCOrNull(config.connection.verifyCrt);
+        espConfig.broker.verification.certificate_len = config.connection.verifyCrt.length();
+        espConfig.credentials.username = config.auth.username.c_str();
+        espConfig.credentials.client_id = clientId.c_str();
+        espConfig.credentials.authentication.password = this->stringToCOrNull(config.auth.password);
+        espConfig.credentials.authentication.certificate = this->stringToCOrNull(config.auth.crt);
+        espConfig.credentials.authentication.certificate_len = config.auth.crt.length();
+        espConfig.credentials.authentication.key = this->stringToCOrNull(config.auth.crtKey);
+        espConfig.credentials.authentication.key_len = config.auth.crtKey.length();
+        espConfig.session.last_will.topic = this->stringToCOrNull(config.lastWill.topic);
+        espConfig.session.last_will.msg = this->stringToCOrNull(config.lastWill.msg);
+        espConfig.session.last_will.qos = config.lastWill.qos;
+        espConfig.session.last_will.retain = config.lastWill.retain;
+        espConfig.session.keepalive = config.connection.keepalive;
+        espConfig.session.disable_keepalive = config.connection.keepalive <= 0;
+
+        // Initialize client
+        m_mqtt = esp_mqtt_client_init(&espConfig);
+
+        // Register event handler
+        ESP_ERROR_CHECK(
+            esp_mqtt_client_register_event(
+                static_cast<esp_mqtt_client_handle_t>(m_mqtt),
+                static_cast<esp_mqtt_event_id_t>(ESP_EVENT_ANY_ID),
+                _eventHandler,
+                this
+            )
+        );
+
+        // Start the client
+        ESP_ERROR_CHECK(
+            esp_mqtt_client_start(static_cast<esp_mqtt_client_handle_t>(m_mqtt))
+        );
+
+        // Wait until connected
+        auto future = m_connectingPromise.get_future();
+        SPSP_LOGI("Attempting connection with timeout %lld seconds",
+                MQTT_CONNECT_TIMEOUT.count());
+
+        // Block
+        if (future.wait_for(MQTT_CONNECT_TIMEOUT) == std::future_status::timeout) {
+            // Connection timeout
+            SPSP_LOGE("Connection timeout");
+            throw MQTTConnectionError();
+        }
+
         SPSP_LOGI("Initialized");
     }
 
     Layer::~Layer()
     {
+        auto mqtt = static_cast<esp_mqtt_client_handle_t>(m_mqtt);
+
+        ESP_ERROR_CHECK(esp_mqtt_client_stop(mqtt));
+        ESP_ERROR_CHECK(esp_mqtt_client_destroy(mqtt));
+
         SPSP_LOGI("Deinitialized");
+    }
+
+    void Layer::reconnect()
+    {
+        auto mqtt = static_cast<esp_mqtt_client_handle_t>(m_mqtt);
+        ESP_ERROR_CHECK(esp_mqtt_client_reconnect(mqtt));
     }
 
     bool Layer::publish(const std::string src, const std::string topic,
                         const std::string payload)
     {
-        SPSP_LOGD("Publish: payload %s to topic %s from %s",
+        SPSP_LOGD("Publish: payload '%s' to topic '%s' from '%s'",
                   payload.c_str(), topic.c_str(), src.c_str());
+
+        std::string topicExtended = m_pubTopicPrefix + "/" + src + "/" + topic;
+
+        // No-blocking publish
+        auto mqtt = static_cast<esp_mqtt_client_handle_t>(m_mqtt);
+        esp_mqtt_client_enqueue(mqtt, topicExtended.c_str(), payload.c_str(),
+                                payload.length(), m_qos, m_retain, true);
+
         return true;
     }
 
     bool Layer::subscribe(const std::string topic)
     {
-        SPSP_LOGD("Subscribe: %s", topic.c_str());
-        return true;
+        SPSP_LOGD("Subscribe to topic '%s'", topic.c_str());
+
+        // Subscribe (blocks)
+        auto mqtt = static_cast<esp_mqtt_client_handle_t>(m_mqtt);
+        return esp_mqtt_client_subscribe(mqtt, topic.c_str(), m_qos) > 0;
     }
 
     bool Layer::unsubscribe(const std::string topic)
     {
-        SPSP_LOGD("Unsubscribe: %s", topic.c_str());
-        return true;
+        SPSP_LOGD("Unsubscribe from topic '%s'", topic.c_str());
+        
+        // Unsubscribe (blocks)
+        auto mqtt = static_cast<esp_mqtt_client_handle_t>(m_mqtt);
+        return esp_mqtt_client_unsubscribe(mqtt, topic.c_str()) > 0;
     }
 }
--- include/spsp_mqtt.hpp
+++ include/spsp_mqtt.hpp
@@ -9,29 +9,109 @@
 
 #pragma once
 
+#include <exception>
+#include <future>
+
 #include "spsp_layers.hpp"
+#include "spsp_node.hpp"
 
 namespace SPSP::FarLayers::MQTT
 {
+    const char* const MQTT_CLIENT_ID_PREFIX = "spsp_";           //!< Default client ID prefix
+    const auto MQTT_CONNECT_TIMEOUT = std::chrono::seconds(10);  //!< Timeout for connecting to broker
+
+    /**
+     * @brief MQTT connection error
+     * 
+     * Thrown when `MQTT_CONNECT_TIMEOUT` expires before successful connection.
+     */
+    class MQTTConnectionError : public std::exception {};
+
+    /**
+     * @brief MQTT client configuration
+     * 
+     */
+    struct ClientConfig
+    {
+        struct Connection
+        {
+            std::string uri;        //!< Complete URI to connect to broker
+                                    //!< e.g. mqtt://username:password@mqtt.eclipseprojects.io:1884/path
+            std::string verifyCrt;  //!< Verification TLS certificate (if TLS is used)
+            int keepalive = 120;    //!< Keepalive interval in seconds (set to 0 to disable keepalive)
+            int qos = 0;            //!< QoS for sent messages
+            bool retain = false;    //!< Retain flag for sent messages
+        };
+
+        struct Auth
+        {
+            std::string username;  //!< Username for connection (can also be set by URI)
+            std::string password;  //!< Password for connection (can also be set by URI)
+            std::string clientId;  //!< Client ID (default is: `spsp_xxx`, where `xxx` is MAC address)
+            std::string crt;       //!< Authentication TLS certificate (if needed).
+                                   //!< If set, also `authKey` must be provided.
+            std::string crtKey;    //!< Private key for TLS authentication
+        };
+
+        struct LastWill
+        {
+            std::string topic;    //!< LWT topic
+            std::string msg;      //!< LWT message
+            int qos = 0;          //!< LWT QoS
+            bool retain = false;  //!< LWT retain flag
+        };
+
+        std::string pubTopicPrefix = "spsp";  //!< Topic format for publishing is:
+                                              //!< %TOPIC_PREFIX%/%ADDR%/%MSG_TOPIC%
+                                              //!< TODO: use C++20 format
+        Connection connection;
+        Auth auth;
+        LastWill lastWill;
+    };
+
     /**
      * @brief MQTT far layer
      * 
      */
     class Layer : public SPSP::IFarLayer
     {
+        void* m_mqtt;                            //!< MQTT client handle
+        std::string m_pubTopicPrefix;            //!< Topic prefix (see `ClientConfig`)
+        int m_qos = 0;                           //!< QoS for sent messages
+        bool m_retain = false;                   //!< Retain flag for sent messages
+        std::promise<void> m_connectingPromise;  //!< Promise to block until successful connection is made
+
     public:
         /**
          * @brief Constructs a new MQTT layer object
          * 
          * Requires already initialized WiFi (with IP address).
+         * 
+         * Block until connection is successfully made.
+         * May throw `MQTTConnectionError`.
+         * 
+         * @param config Configuration
          */
-        Layer();
+        Layer(const ClientConfig config);
 
         /**
          * @brief Destroys MQTT layer object
          * 
          */
         ~Layer();
+
+        /**
+         * @brief Signalizes successful initial connection to broker
+         * 
+         */
+        void connected() { m_connectingPromise.set_value(); }
+
+        /**
+         * @brief Reconnects to broker
+         * 
+         * This is for event handler.
+         */
+        void reconnect();
 
     protected:
         /**
@@ -67,5 +147,16 @@ namespace SPSP::FarLayers::MQTT
          * @return false Unsubscribe failed
          */
         bool unsubscribe(const std::string topic);
+
+        /**
+         * @brief Helper to convert `std::string` to C string or `nullptr`
+         * 
+         * @param str String
+         * @return If string is empty, `nullptr`, otherwise C string. 
+         */
+        inline static const char* stringToCOrNull(const std::string& str)
+        {
+            return str.empty() ? nullptr : str.c_str();
+        }
     };
 } // namespace SPSP::FarLayers::MQTT
