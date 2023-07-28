52b9c107e34e9eeed3c8c11fc6cff73c57f28c5f
Dávid Benko <davidbenko@davidbenko.dev>
2023-07-28 11:31:11 +0200

    LocalAddrMAC: create container

---
+++ esp-idf/src/local_addr_mac.cpp
@@ -0,0 +1,54 @@
+/**
+ * @file local_addr_mac.cpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief Local layer address container for MAC address
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "esp_mac.h"
+
+#include "spsp_local_addr_mac.hpp"
+
+namespace SPSP
+{
+    LocalAddrMAC::LocalAddrMAC(const uint8_t* mac)
+    {
+        // Internal representation
+        addr = std::vector<uint8_t>(mac, mac + MAC_LEN);
+
+        // Printable string
+        char macStr[2*MAC_LEN + 1];
+        sprintf(macStr, "%02x%02x%02x%02x%02x%02x", MAC2STR(mac));
+        str = std::string(macStr);
+    }
+
+    LocalAddrMAC LocalAddrMAC::local()
+    {
+        uint8_t mac[MAC_LEN];
+        ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac));
+        return LocalAddrMAC(mac);
+    }
+
+    LocalAddrMAC LocalAddrMAC::zeroes()
+    {
+        uint8_t mac[MAC_LEN];
+        memset(mac, 0x00, MAC_LEN);
+        return LocalAddrMAC(mac);
+    }
+
+    LocalAddrMAC LocalAddrMAC::broadcast()
+    {
+        uint8_t mac[MAC_LEN];
+        memset(mac, 0xFF, MAC_LEN);
+        return LocalAddrMAC(mac);
+    }
+
+    void LocalAddrMAC::toMAC(uint8_t* mac) const
+    {
+        for (unsigned i = 0; i < addr.size(); i++) {
+            mac[i] = addr[i];
+        }
+    }
+}
---
+++ include/spsp_local_addr_mac.hpp
@@ -0,0 +1,68 @@
+/**
+ * @file spsp_local_addr_mac.hpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief Local layer address container for MAC address
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+#include "spsp_local_addr.hpp"
+
+namespace SPSP
+{
+    /**
+     * @brief Length of MAC address in bytes
+     * 
+     * Should be the same as `ESP_NOW_ETH_ALEN`, but plaform independent.
+     */
+    constexpr const size_t MAC_LEN = 6;
+
+    /**
+     * @brief Local layer address container for MAC address
+     * 
+     * MAC as addess type is very common, so implement everything here.
+     */
+    struct LocalAddrMAC : public LocalAddr
+    {
+        /**
+         * @brief Constructs a new object
+         * 
+         * @param mac MAC address
+         */
+        LocalAddrMAC(const uint8_t* mac);
+
+        /**
+         * @brief Constructs a new object from MAC address of this node
+         * 
+         * @return New `LocalAddrMAC` object
+         */
+        static LocalAddrMAC local();
+
+        /**
+         * @brief Constructs a new object from 00:00:00:00:00:00 MAC address
+         * 
+         * @return New `LocalAddrMAC` object
+         */
+        static LocalAddrMAC zeroes();
+
+        /**
+         * @brief Constructs a new object from broadcast MAC address
+         * 
+         * @return New `LocalAddrMAC` object
+         */
+        static LocalAddrMAC broadcast();
+
+        /**
+         * @brief Converts `LocalAddrMAC` to MAC itself
+         * 
+         * @param mac MAC address storage pointer
+         */
+        void toMAC(uint8_t* mac) const;
+    };
+}
