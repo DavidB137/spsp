9fbb359b2ffcfe75e4a7207b19ceaed86ec28be0
Dávid Benko <davidbenko@davidbenko.dev>
2023-08-03 10:28:35 +0200

    Timer: create

---
+++ esp-idf/src/timer.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file mqtt.cpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief MQTT far layer for SPSP
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
+
+#include "spsp_timer.hpp"
+
+namespace SPSP
+{
+    void _timer_callback(TimerHandle_t timer)
+    {
+        Timer* ctx = static_cast<Timer*>(pvTimerGetTimerID(timer));
+        ctx->callCb();
+    }
+
+    Timer::Timer(const std::chrono::milliseconds interval,
+                 std::function<void()> cb)
+        : m_cb{cb}
+    {
+        m_timer = xTimerCreate("SPSP::Timer", pdMS_TO_TICKS(interval.count()),
+                               pdTRUE, this, _timer_callback);
+
+        // Timer can't be created
+        if (m_timer == nullptr) {
+            throw TimerError();
+        }
+
+        // Timer can't be started
+        if (xTimerStart(static_cast<TimerHandle_t>(m_timer), 0) == pdFAIL) {
+            throw TimerError();
+        }
+    }
+
+    Timer::~Timer()
+    {
+        TimerHandle_t timer = static_cast<TimerHandle_t>(m_timer);
+        xTimerStop(timer, 0);
+        xTimerDelete(timer, 0);
+    }
+}
---
+++ include/spsp_timer.hpp
@@ -0,0 +1,64 @@
+/**
+ * @file spsp_timer.hpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief Timer for SPSP purposes
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+
+#include <chrono>
+#include <exception>
+#include <functional>
+
+namespace SPSP
+{
+    /**
+     * @brief Timer error
+     * 
+     * Thrown when timer cannot be created or started.
+     */
+    class TimerError : public std::exception {};
+
+    /**
+     * @brief Simple timer for SPSP purposes
+     * 
+     * (... but you can also use it outside of SPSP.)
+     * 
+     * Basically it's just a plaform-dependent wrapper.
+     * On ESP-based platforms wraps FreeRTOS timers.
+     * 
+     */
+    class Timer
+    {
+    protected:
+        void* m_timer;               //!< Timer handle (platform dependent)
+        std::function<void()> m_cb;  //!< Callback
+
+    public:
+        /**
+         * @brief Constructs a new timer
+         * 
+         * First execution of `cb` will be after first `interval` expires
+         * (not immediately).
+         * 
+         * @param interval Interval of timer
+         * @param cb Callback (`std::bind` can be used)
+         */
+        Timer(const std::chrono::milliseconds interval, std::function<void()> cb);
+
+        /**
+         * @brief Destroys the timer
+         * 
+         */
+        ~Timer();
+
+        /**
+         * @brief Calls stored callback
+         * 
+         */
+        void callCb() { m_cb(); }
+    };
+} // namespace SPSP
