d56158e8503bea3145ee730b046aea8f8db47e99
Dávid Benko <davidbenko@davidbenko.dev>
2023-08-20 11:09:32 +0200

    WiFi: create interface for ESPNOW requirements
    ESPNOW will require instance of WiFi adapter implementing this
    in the future.

---
+++ common/include/spsp_wifi_espnow.hpp
@@ -0,0 +1,47 @@
+/**
+ * @file spsp_wifi_espnow.hpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief WiFi interface for ESP-NOW
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#pragma once
+
+#include <cstdint>
+
+#include "spsp_wifi_country_restrictions.hpp"
+
+namespace SPSP::WiFi
+{
+    /**
+     * @brief Requirements of ESP-NOW from WiFi instance
+     *
+     * Should be implemented by platform-specific WiFi adapter.
+     */
+    class IESPNOW
+    {
+        /**
+         * @brief Gets current WiFi channel
+         * 
+         * @return Current WiFi channel
+         */
+        virtual uint8_t getChannel() = 0;
+
+        /**
+         * @brief Sets current WiFi channel
+         * 
+         * @param ch New current WiFi channel
+         */
+        virtual void setChannel(uint8_t ch) = 0;
+
+        /**
+         * @brief Gets currently set country restrictions of WiFi adapter
+         * 
+         * @return Country restrictions
+         */
+        virtual const CountryRestrictions getCountryRestrictions() = 0;
+    };
+} // namespace SPSP::WiFi
+
