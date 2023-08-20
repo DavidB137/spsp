fdb071de6719c1aa0c298f6eb19406fd0781cfc2
Dávid Benko <davidbenko@davidbenko.dev>
2023-08-20 10:38:36 +0200

    Add "all" header files

---
+++ common/include/spsp_common.hpp
@@ -0,0 +1,20 @@
+/**
+ * @file spsp_common.hpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief Entrypoint include file for platform-independent headers
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "spsp_bridge.hpp"
+#include "spsp_client.hpp"
+#include "spsp_espnow.hpp"
+#include "spsp_exception.hpp"
+#include "spsp_layers.hpp"
+#include "spsp_local_addr_mac.hpp"
+#include "spsp_local_addr.hpp"
+#include "spsp_mqtt.hpp"
+#include "spsp_node.hpp"
+#include "spsp_timer.hpp"
+#include "spsp_version.hpp"
---
+++ espidf/include/spsp.hpp
@@ -0,0 +1,11 @@
+/**
+ * @file spsp.hpp
+ * @author Dávid Benko (davidbenko@davidbenko.dev)
+ * @brief Entrypoint include file for common and ESP platform headers
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include "spsp_common.hpp"
+#include "spsp_wifi.hpp"
