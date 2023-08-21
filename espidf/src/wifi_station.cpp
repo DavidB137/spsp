57efd01ba19d2be013b5031d2219de1e502a16d7
Dávid Benko <davidbenko@davidbenko.dev>
2023-08-21 13:58:18 +0200

    WiFi: replace `ESP_ERROR_CHECK` with exceptions

--- espidf/src/wifi_station.cpp
+++ espidf/src/wifi_station.cpp
@@ -13,6 +13,7 @@
 #include "esp_wifi.h"
 #include "nvs_flash.h"
 
+#include "spsp_exception_check.hpp"
 #include "spsp_logger.hpp"
 #include "spsp_wifi_station.hpp"
 
@@ -26,13 +27,11 @@ namespace SPSP::WiFi
         // Mutex
         const std::lock_guard<std::mutex> lock(m_mutex);
 
-        // Don't do anything if already initialized
-        if (m_initialized) return;
-
         m_config = config;
 
         // Create event loop
-        ESP_ERROR_CHECK(esp_event_loop_create_default());
+        SPSP_ERROR_CHECK(esp_event_loop_create_default(),
+                         ConnectionError("Create event loop failed"));
 
         // Initialize NVS and network
         this->initNVS();
@@ -43,19 +42,22 @@ namespace SPSP::WiFi
 
         // Init configuration
         wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-        ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-        ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
-        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+        SPSP_ERROR_CHECK(esp_wifi_init(&cfg), ConnectionError("WiFi init failed"));
+        SPSP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM),
+                         ConnectionError("Set WiFi storage failed"));
+        SPSP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA),
+                         ConnectionError("Set WiFi mode failed"));
 
         // Use current config settings
         this->initWiFiConfig();
 
         // Start WiFi
-        ESP_ERROR_CHECK(esp_wifi_start());
+        SPSP_ERROR_CHECK(esp_wifi_start(), ConnectionError("WiFi start failed"));
 
         // Set TX power
         if (config.maxTxPower != TX_POWER_DEFAULT) {
-            ESP_ERROR_CHECK(esp_wifi_set_max_tx_power(4 * config.maxTxPower));
+            SPSP_ERROR_CHECK(esp_wifi_set_max_tx_power(4 * config.maxTxPower),
+                             ConnectionError("Set max TX power failed"));
         }
 
         // Wait until IP is received (only if SSID is not empty - i.e. this is
@@ -85,14 +87,9 @@ namespace SPSP::WiFi
         // Mutex
         const std::lock_guard<std::mutex> lock(m_mutex);
 
-        // Don't do anything if already deinitialized
-        if (!m_initialized) return;
-
-        ESP_ERROR_CHECK(esp_wifi_stop());
-        ESP_ERROR_CHECK(esp_wifi_deinit());
-        ESP_ERROR_CHECK(esp_event_loop_delete_default());
-
-        m_initialized = false;
+        if (esp_wifi_stop() != ESP_OK) return;
+        if (esp_wifi_deinit() != ESP_OK) return;
+        if (esp_event_loop_delete_default() != ESP_OK) return;
 
         SPSP_LOGI("Deinitialized");
     }
@@ -104,7 +101,8 @@ namespace SPSP::WiFi
 
         uint8_t ch;
         wifi_second_chan_t sec;
-        ESP_ERROR_CHECK(esp_wifi_get_channel(&ch, &sec));
+        SPSP_ERROR_CHECK(esp_wifi_get_channel(&ch, &sec),
+                         ConnectionError("Get WiFi channel failed"));
         return ch;
     }
 
@@ -113,7 +111,8 @@ namespace SPSP::WiFi
         // Mutex
         const std::lock_guard lock(m_mutex);
 
-        ESP_ERROR_CHECK(esp_wifi_set_channel(ch, WIFI_SECOND_CHAN_NONE));
+        SPSP_ERROR_CHECK(esp_wifi_set_channel(ch, WIFI_SECOND_CHAN_NONE),
+                         ConnectionError("Set WiFi channel failed"));
         SPSP_LOGI("Set channel %d", ch);
     }
 
@@ -126,7 +125,8 @@ namespace SPSP::WiFi
         c.schan = rest.low;
         c.nchan = rest.high - rest.low + 1;
 
-        ESP_ERROR_CHECK(esp_wifi_set_country(&c));
+        SPSP_ERROR_CHECK(esp_wifi_set_country(&c),
+                         ConnectionError("Set WiFi country restrictions failed"));
 
         SPSP_LOGI("Set channel restrictions: %d - %d", rest.low, rest.high);
     }
@@ -137,7 +137,8 @@ namespace SPSP::WiFi
         const std::lock_guard lock(m_mutex);
 
         wifi_country_t c;
-        ESP_ERROR_CHECK(esp_wifi_get_country(&c));
+        SPSP_ERROR_CHECK(esp_wifi_get_country(&c),
+                         ConnectionError("Get country restrictions failed"));
 
         ChannelRestrictions rest = {};
         rest.low = c.schan;
@@ -151,7 +152,8 @@ namespace SPSP::WiFi
             return;
         }
 
-        ESP_ERROR_CHECK(esp_netif_create_ip6_linklocal(m_netIf));
+        SPSP_ERROR_CHECK(esp_netif_create_ip6_linklocal(m_netIf),
+                         ConnectionError("Create IPv6 link-local failed"));
     }
 
     void Station::initNVS()
@@ -160,11 +162,12 @@ namespace SPSP::WiFi
 
         if (nvsInitCode == ESP_ERR_NVS_NO_FREE_PAGES ||
             nvsInitCode == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-            ESP_ERROR_CHECK(nvs_flash_erase());
+            SPSP_ERROR_CHECK(nvs_flash_erase(),
+                             ConnectionError("NVS flash erase failed"));
             nvsInitCode = nvs_flash_init();
         }
 
-        ESP_ERROR_CHECK(nvsInitCode);
+        SPSP_ERROR_CHECK(nvsInitCode, ConnectionError("NVS init failed"));
 
         SPSP_LOGI("NVS initialized");
     }
@@ -174,7 +177,8 @@ namespace SPSP::WiFi
         // Don't do anything if SSID is empty
         if (m_config.ssid.length() == 0) return;
 
-        ESP_ERROR_CHECK(esp_netif_init());
+        SPSP_ERROR_CHECK(esp_netif_init(),
+                         ConnectionError("Network interface init failed"));
 
         // Create net interface
         m_netIf = esp_netif_create_default_wifi_sta();
@@ -183,7 +187,7 @@ namespace SPSP::WiFi
         std::string hostname = m_config.hostnamePrefix;
         uint8_t mac[8];
         char macStr[16];
-        ESP_ERROR_CHECK(esp_efuse_mac_get_default(mac));
+        esp_efuse_mac_get_default(mac);
         sprintf(macStr, "%02x%02x%02x%02x%02x%02x", MAC2STR(mac));
         hostname += macStr;
         esp_netif_set_hostname(m_netIf, hostname.c_str());
@@ -194,14 +198,16 @@ namespace SPSP::WiFi
     void Station::registerEventHandlers()
     {
         // WiFi events
-        ESP_ERROR_CHECK(esp_event_handler_instance_register(
+        SPSP_ERROR_CHECK(esp_event_handler_instance_register(
             WIFI_EVENT, ESP_EVENT_ANY_ID, &Station::eventHandlerWiFi,
-            this, nullptr));
+            this, nullptr),
+            ConnectionError("Register WiFi event handler failed"));
 
         // IP events
-        ESP_ERROR_CHECK(esp_event_handler_instance_register(
+        SPSP_ERROR_CHECK(esp_event_handler_instance_register(
             IP_EVENT, ESP_EVENT_ANY_ID, &Station::eventHandlerIP,
-            this, nullptr));
+            this, nullptr),
+            ConnectionError("Register IP event handler failed"));
     }
 
     void Station::initWiFiConfig()
@@ -229,7 +235,8 @@ namespace SPSP::WiFi
             espWiFiConfig.sta.owe_enabled = true;
             espWiFiConfig.sta.sae_pwe_h2e = WPA3_SAE_PWE_BOTH;
 
-            ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &espWiFiConfig));
+            SPSP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &espWiFiConfig),
+                             ConnectionError("Set WiFi config failed"));
 
             // No power saving for bridge (IMPORTANT!)
             esp_wifi_set_ps(WIFI_PS_NONE);
