4d1484abc32faf49a4a610b3193b77025ef2fdd0
Dávid Benko <davidbenko@davidbenko.dev>
2023-08-21 22:30:27 +0200

    Test: fix `spsp_random.hpp`

--- test/include/spsp_random.hpp
+++ test/include/spsp_random.hpp
@@ -9,6 +9,7 @@
 
 #pragma once
 
+#include <cstdint>
 #include <cstdlib>
 
 #include "spsp_random_if.hpp"
@@ -24,8 +25,10 @@ namespace SPSP
     public:
         inline void bytes(void* buf, size_t len) const noexcept
         {
+            uint8_t* buf8 = static_cast<uint8_t*>(buf);
+
             for (size_t i = 0; i < len; i++) {
-                buf[i] = rand();
+                buf8[i] = rand();
             }
         }
     };
