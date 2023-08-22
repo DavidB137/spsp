ced01fb2e6922ffd1654f08bfa5f6f6285175405
Dávid Benko <davidbenko@davidbenko.dev>
2023-08-22 21:54:37 +0200

    Timer: generalize
    Make timer platform-independent.

--- include/common/spsp_timer.hpp
+++ include/common/spsp_timer.hpp
@@ -10,33 +10,29 @@
 #pragma once
 
 #include <chrono>
+#include <condition_variable>
 #include <exception>
 #include <functional>
+#include <mutex>
+#include <thread>
 
 namespace SPSP
 {
-    /**
-     * @brief Timer error
-     *
-     * Thrown when timer cannot be created or started.
-     */
-    class TimerError : public std::exception {};
-
     /**
      * @brief Simple timer for SPSP purposes
      *
      * (... but you can also use it outside of SPSP.)
      *
-     * Basically it's just a plaform-dependent wrapper.
-     * On ESP-based platforms wraps FreeRTOS timers.
-     *
      */
     class Timer
     {
-    protected:
-        void* m_timer;               //!< Timer handle (platform dependent)
-        std::function<void()> m_cb;  //!< Callback
-        bool cbInNewThread = true;   //!< Whether to call callback in new thread
+        std::chrono::milliseconds m_interval;              //!< Timer interval
+        std::chrono::system_clock::time_point m_nextExec;  //!< Next execution time point
+        bool m_run;                                        //!< Whether to continue running
+        std::function<void()> m_cb;                        //!< Callback
+        std::thread m_thread;                              //!< Handler thread
+        std::condition_variable m_cv;                      //!< Conditional variable (to sync destruction of handler thread)
+        std::mutex m_mutex;                                //!< Mutex for conditional variable
 
     public:
         /**
@@ -49,8 +45,8 @@ namespace SPSP
          * @param cb Callback (`std::bind` can be used)
          * @param cbInNewThread Whether to call callback in new thread (prevents stack overflows)
          */
-        Timer(const std::chrono::milliseconds interval, std::function<void()> cb,
-              bool cbInNewThread = true);
+        Timer(const std::chrono::milliseconds interval,
+              std::function<void()> cb);
 
         /**
          * @brief Destroys the timer
@@ -58,11 +54,11 @@ namespace SPSP
          */
         ~Timer();
 
+    protected:
         /**
-         * @brief Calls stored callback
+         * @brief Timer handler thread
          *
-         * A new thread is spawned for this call.
          */
-        void callCb();
+        void handlerThread();
     };
 } // namespace SPSP
