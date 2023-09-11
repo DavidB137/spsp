c49cff84512c524c5c170b515dfaa50e5dba33d4
Dávid Benko <davidbenko@davidbenko.dev>
2023-09-11 10:26:41 +0200

    ESPNOW: add more tests for connecting to bridge

--- test/tests/espnow.cpp
+++ test/tests/espnow.cpp
@@ -17,15 +17,16 @@ using namespace std::chrono_literals;
 using LocalAddrT = SPSP::LocalLayers::ESPNOW::LocalAddrT;
 using LocalMessageT = SPSP::LocalLayers::ESPNOW::LocalMessageT;
 
-const uint8_t ADDR_LOCAL_MAC[6] = {0x12, 0x23, 0x34, 0x45, 0x56, 0x00};
 const uint8_t ADDR_PEER_MAC[6] = {0x12, 0x23, 0x34, 0x45, 0x56, 0x01};
-const LocalAddrT ADDR_LOCAL = ADDR_LOCAL_MAC;
+const uint8_t ADDR_PEER_MAC2[6] = {0x12, 0x23, 0x34, 0x45, 0x56, 0x02};
 const LocalAddrT ADDR_PEER = ADDR_PEER_MAC;
+const LocalAddrT ADDR_PEER2 = ADDR_PEER_MAC2;
 const std::string TOPIC = "abc";
 const std::string PAYLOAD = "123";
 const LocalLayers::ESPNOW::Config CONF = {
     .ssid = 0x01020304,
-    .password = std::string(32, 0x48)
+    .password = std::string(32, 0x48),
+    .connectToBridgeChannelWaiting = 50ms
 };
 
 const LocalMessageT MSG_BASE = {
@@ -33,6 +34,16 @@ const LocalMessageT MSG_BASE = {
     .addr = ADDR_PEER,
     .topic = TOPIC,
     .payload = PAYLOAD
+};
+
+const LocalMessageT MSG_BASE_PROBE_RES = {
+    .type = LocalMessageType::PROBE_RES,
+    .addr = ADDR_PEER,
+};
+
+const LocalMessageT MSG_BASE_PROBE_RES2 = {
+    .type = LocalMessageType::PROBE_RES,
+    .addr = ADDR_PEER2,
 };
 
 class AdapterSendSuccess : public LocalLayers::ESPNOW::Adapter
@@ -62,6 +73,18 @@ class AdapterSendFail : public LocalLayers::ESPNOW::Adapter
     {
         std::thread t(this->getSendCb(), dst, false);
         t.detach();
+    }
+};
+
+// ESPNOW with support for direct receive of message
+class ESPNOWRcvDir : public LocalLayers::ESPNOW::ESPNOW
+{
+public:
+    using LocalLayers::ESPNOW::ESPNOW::ESPNOW;
+
+    void receiveDirect(const LocalMessageT& msg, int rssi)
+    {
+        this->receive(msg, rssi);
     }
 };
 
@@ -214,4 +237,129 @@ TEST_CASE("Connect to bridge fail - connection info doesn't change", "[ESPNOW]")
     CHECK(brInfo.ch == 0);
 }
 
-// TODO: more connect to bridge tests
+TEST_CASE("Connect to bridge success", "[ESPNOW]") {
+    WiFi::Dummy wifi{};
+    AdapterSendSuccess adapter{};
+    ESPNOWRcvDir espnow{adapter, wifi, CONF};
+
+    const auto channelRestrictions = wifi.getChannelRestrictions();
+    LocalLayers::ESPNOW::BridgeConnInfoRTC brInfo = {};
+
+    SECTION("One response on last channel") {
+        const auto probeResWaitTime = CONF.connectToBridgeChannelWaiting * (channelRestrictions.high - channelRestrictions.low) + 10ms;
+
+        // Receive message simulator
+        std::thread t1([&espnow, &probeResWaitTime]() {
+            std::this_thread::sleep_for(probeResWaitTime);
+            espnow.receiveDirect(MSG_BASE_PROBE_RES, -50);
+        });
+
+        std::thread t2([&espnow, &brInfo]() {
+            REQUIRE(espnow.connectToBridge(nullptr, &brInfo));
+        });
+
+        t1.join();
+        t2.join();
+
+        CHECK(brInfo.ch == channelRestrictions.high);
+    }
+
+    SECTION("One response on first channel") {
+        // Receive message simulator
+        std::thread t1([&espnow]() {
+            std::this_thread::sleep_for(10ms);
+            espnow.receiveDirect(MSG_BASE_PROBE_RES, -50);
+        });
+
+        std::thread t2([&espnow, &brInfo]() {
+            REQUIRE(espnow.connectToBridge(nullptr, &brInfo));
+        });
+
+        t1.join();
+        t2.join();
+
+        CHECK(brInfo.ch == channelRestrictions.low);
+    }
+
+    SECTION("One response on middle channel") {
+        // Receive message simulator
+        std::thread t1([&espnow]() {
+            std::this_thread::sleep_for(CONF.connectToBridgeChannelWaiting + 10ms);
+            espnow.receiveDirect(MSG_BASE_PROBE_RES, -50);
+        });
+
+        std::thread t2([&espnow, &brInfo]() {
+            REQUIRE(espnow.connectToBridge(nullptr, &brInfo));
+        });
+
+        t1.join();
+        t2.join();
+
+        CHECK(brInfo.ch == channelRestrictions.low + 1);
+    }
+
+    SECTION("Three responses from two bridges on same channel") {
+        // Receive message simulator
+        std::thread t1([&espnow]() {
+            std::this_thread::sleep_for(10ms);
+            espnow.receiveDirect(MSG_BASE_PROBE_RES, -50);
+            std::this_thread::sleep_for(10ms);
+            espnow.receiveDirect(MSG_BASE_PROBE_RES2, -48);
+            std::this_thread::sleep_for(10ms);
+            espnow.receiveDirect(MSG_BASE_PROBE_RES, -45);
+        });
+
+        std::thread t2([&espnow, &brInfo]() {
+            REQUIRE(espnow.connectToBridge(nullptr, &brInfo));
+        });
+
+        t1.join();
+        t2.join();
+
+        CHECK(brInfo.ch == channelRestrictions.low);
+    }
+
+    SECTION("Two responses with same RSSI") {
+        // Receive message simulator
+        std::thread t1([&espnow]() {
+            std::this_thread::sleep_for(10ms);
+            espnow.receiveDirect(MSG_BASE_PROBE_RES, -30);
+            std::this_thread::sleep_for(CONF.connectToBridgeChannelWaiting);
+            espnow.receiveDirect(MSG_BASE_PROBE_RES2, -40);
+        });
+
+        std::thread t2([&espnow, &brInfo]() {
+            REQUIRE(espnow.connectToBridge(nullptr, &brInfo));
+        });
+
+        t1.join();
+        t2.join();
+
+        CHECK(brInfo.ch == channelRestrictions.low);
+    }
+
+    LocalAddrT brAddr = brInfo.addr;
+
+    // Check correct bridge address and channel
+    CHECK(static_cast<LocalAddr>(brAddr) == static_cast<LocalAddr>(ADDR_PEER));
+}
+
+TEST_CASE("Connect to bridge success - reconnect to bridge", "[ESPNOW]") {
+    WiFi::Dummy wifi{};
+    AdapterSendFail adapter{};
+    LocalLayers::ESPNOW::ESPNOW espnow{adapter, wifi, CONF};
+
+    // Populate retained bridge info
+    LocalLayers::ESPNOW::BridgeConnInfoRTC brInfo = {};
+    LocalLayers::ESPNOW::BridgeConnInfoRTC brInfoNew = {};
+    ADDR_PEER.toMAC(brInfo.addr);
+    brInfo.ch = 1;
+
+    // This should return immediately
+    REQUIRE(espnow.connectToBridge(&brInfo, &brInfoNew));
+
+    // Check new retained bridge info
+    LocalAddrT brAddr = brInfoNew.addr;
+    CHECK(static_cast<LocalAddr>(brAddr) == static_cast<LocalAddr>(ADDR_PEER));
+    CHECK(brInfoNew.ch == 1);
+}
