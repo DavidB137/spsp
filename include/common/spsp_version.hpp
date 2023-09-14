ea82374c67f88c8396aeb6711d54ddd41f8439cd
Dávid Benko <davidbenko@davidbenko.dev>
2023-09-14 16:49:59 +0200

    Version: bump to 0.4.0

--- include/common/spsp_version.hpp
+++ include/common/spsp_version.hpp
@@ -15,5 +15,5 @@ namespace SPSP
      * @brief Version of SPSP
      *
      */
-    constexpr const char* const VERSION = "0.3.0";
+    constexpr const char* const VERSION = "0.4.0";
 } // namespace SPSP
