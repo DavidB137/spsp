d62d5490d3b16af6d7dc6b19420e8de5b3c4bec6
Dávid Benko <davidbenko@davidbenko.dev>
2024-03-16 12:59:14 +0100

    Logger: add `SPSP_LOG_NO_COLORS` flag
    Make it possible to turn off colored logging.

--- include/linux/spsp_logger.hpp
+++ include/linux/spsp_logger.hpp
@@ -27,13 +27,22 @@ namespace SPSP
     extern LogLevel logLevel;
 } // namespace SPSP
 
-#define _SPSP_LOG(level, levelLogStr, fmt, ...) do { \
-        if (SPSP::logLevel <= SPSP::LogLevel::level) { \
-            fprintf(stderr, levelLogStr " %s: " fmt "\n\033[0m", SPSP_LOG_TAG, ##__VA_ARGS__); \
-        } \
+#define _SPSP_LOG(level, levelLogStrPre, levelLogStrPost, fmt, ...)          \
+    do {                                                                     \
+        if (SPSP::logLevel <= SPSP::LogLevel::level) {                       \
+            fprintf(stderr, levelLogStrPre " %s: " fmt "\n" levelLogStrPost, \
+                    SPSP_LOG_TAG, ##__VA_ARGS__);                            \
+        }                                                                    \
     } while (0)
 
-#define SPSP_LOGD(fmt, ...) _SPSP_LOG(DEBUG, "\033[0;34m[D]", fmt, ##__VA_ARGS__)
-#define SPSP_LOGI(fmt, ...) _SPSP_LOG(INFO, "\033[0;36m[I]", fmt, ##__VA_ARGS__)
-#define SPSP_LOGW(fmt, ...) _SPSP_LOG(WARN, "\033[0;33m[W]", fmt, ##__VA_ARGS__)
-#define SPSP_LOGE(fmt, ...) _SPSP_LOG(ERROR, "\033[0;31m[E]", fmt, ##__VA_ARGS__)
+#if SPSP_LOG_NO_COLORS
+    #define SPSP_LOGD(fmt, ...) _SPSP_LOG(DEBUG, "[D]", "", fmt, ##__VA_ARGS__)
+    #define SPSP_LOGI(fmt, ...) _SPSP_LOG(INFO,  "[I]", "", fmt, ##__VA_ARGS__)
+    #define SPSP_LOGW(fmt, ...) _SPSP_LOG(WARN,  "[W]", "", fmt, ##__VA_ARGS__)
+    #define SPSP_LOGE(fmt, ...) _SPSP_LOG(ERROR, "[E]", "", fmt, ##__VA_ARGS__)
+#else
+    #define SPSP_LOGD(fmt, ...) _SPSP_LOG(DEBUG, "\033[0;34m[D]", "\033[0m", fmt, ##__VA_ARGS__)
+    #define SPSP_LOGI(fmt, ...) _SPSP_LOG(INFO,  "\033[0;36m[I]", "\033[0m", fmt, ##__VA_ARGS__)
+    #define SPSP_LOGW(fmt, ...) _SPSP_LOG(WARN,  "\033[0;33m[W]", "\033[0m", fmt, ##__VA_ARGS__)
+    #define SPSP_LOGE(fmt, ...) _SPSP_LOG(ERROR, "\033[0;31m[E]", "\033[0m", fmt, ##__VA_ARGS__)
+#endif
